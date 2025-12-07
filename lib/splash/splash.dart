431e3e31b28edb0f6ed8324d9fb38c518ccf4ec9
Niponpal <niponpal52@gmail.com>
2025-12-07 11:35:34 +0600

    Fix 🚀🚀🚑 SplashScreen

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -12,6 +12,66 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF2AF598),
+              Color(0xFF009EFD),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top:401  ),
+              child: Center(child: Image.asset("assets/ShapSplash.png",width: 80,)),
+            ),
+            SizedBox(height: 210,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 28),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    elevation: 6,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 12),
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already have an account? "),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
   }
 }
