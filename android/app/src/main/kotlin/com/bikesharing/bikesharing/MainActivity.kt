0a92ff33d90657b99697cdaa63b195133cd72f40
Niponpal <niponpal52@gmail.com>
2025-12-07 11:32:07 +0600

    Fix 🚀🚀🚀🚑 Project int

---
+++ android/app/src/main/kotlin/com/bikesharing/bikesharing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bikesharing.bikesharing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:bikesharing/splash/splash.dart';
+
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    )
+  );
+}
